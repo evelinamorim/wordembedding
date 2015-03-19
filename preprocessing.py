9f79f7f19908eab8eb92f3a38a0b443b3d90b7c0
Evelin Amorim <evelin.amorim@hera.speed.dcc.ufmg.br>
2015-03-19 18:27:50 -0300

    Treating wikipedia text

--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #author: evelin amorim
 
 #the preprocessing class perform:
@@ -12,63 +13,69 @@
 import sys
 from mw import xml_dump
 from gensim.models import word2vec
-<<<<<<< HEAD
 import sys
-=======
 import re
->>>>>>> 2c8f686deb2ee6830d4b97690eec9422f3431d62
+import codecs
 
 # [^A-Za-z0-9'-\.]
 class PreProcess:
 
     def __init__(self,train_type='wikipedia'):
-	self.__train_type = train_type
-	self.input_train = None
+        self.__train_type = train_type
+        self.input_train = None
 
     def read_train(self,file_name):
-	"""
-	@input file_name string: The file name for the 
+        """
+        @input file_name string: The file name for the 
 	           training input
-	"""
+        """
 
-	if (self.__train_type=='wikipedia'):
-	    self.read_wikipedia(file_name)
-	elif (self.__train_type=='plain'):
-	    #plain text to train
-	    self.input_train = self.read_plain(file_name)
-	else:
-	    print "Error: Train type not supported"
-	    sys.exit(0)
+        if (self.__train_type=='wikipedia'):
+            self.read_wikipedia(file_name)
+        elif (self.__train_type=='plain'):
+            #plain text to train
+            self.input_train = self.read_plain(file_name)
+        else:
+            print("Error: Train type not supported")
+            sys.exit(0)
 
     def read_plain(self,file_name):
-	"""
-	Read plain text to train
-	TODO: generalizar para outros diferentes do text8 e depois 
-	passar pelo pre-processamento descrito no artigo
-	"""
-	sentences = word2vec.Text8Corpus(file_name)
-	return sentences
-
+        """
+        Read plain text to train
+        TODO: generalizar para outros diferentes do text8 e depois 
+        passar pelo pre-processamento descrito no artigo
+        """
+        sentences = word2vec.Text8Corpus(file_name)
+        return sentences
 
     def read_wikipedia(self,file_name):
-	"""
-	Read and pre-process wikipedia dump file
+        """
+        Read and pre-process wikipedia dump file
 
-	@input file_name string: The file name for the 
+        @input file_name string: The file name for the 
 	           training input
-	"""
-	dump = xml_dump.Iterator.from_file(open(file_name))
+        """
+        dump = xml_dump.Iterator.from_file(codecs.open(file_name,"r","utf-8"))
 
-	for page in dump:
-	    for revision in page:
-                print("{0} {1}".format(revision.id, page_id,revision.text))
-		break
+        i = 1
+        #pages that dont need to be processed
+        redirect_page = re.compile('#REDIRECT')
+        for page in dump:
+            print("*******************************")
+            for revision in page:
+                if (not(redirect_page.match(revision.text))):
+                    print("{0} ".format( revision.text.encode('UTF-8')))
+                    print("{0} ".format(page.title))
+
+            if (i==10): break
+            print()
+            i = i+1
 
     def write_wikipedia():
-	"""
-	Write de preprocessed wikipedia
-	"""
-	pass
+        """
+         Write de preprocessed wikipedia
+        """
+        pass
 
 
 if __name__ == "__main__":
---
+++ util.py
@@ -0,0 +1,14 @@
+
+def process_wiki_str(str_w):
+    new_str_w = ''
+    i = 0
+    n = len(str_w)
+    while (i<n):
+        if ('a' <= str_w[i] and str_w[i]<='z'):
+             new_str_w +=str_w[i]
+        elif ('A' <= str_w[i] and str_w[i]<='Z'):
+             new_str_w +=str_w[i].lower()
+        elif ('0' <= str_w[i] and str_w[i]<='9')
+             new_str_w +=str_w[i]
+        elif ()
+        i = i+1
