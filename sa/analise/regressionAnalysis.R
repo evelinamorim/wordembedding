25486bc8b0ea89a9702a022ee5138f00e76ac56f
Evelin Amorim <evelinamorim@note-adriano.(none)>
2015-07-28 16:03:59 -0300

    training regression model

--- sa/analise/regressionAnalysis.R
+++ sa/analise/regressionAnalysis.R
@@ -9,28 +9,55 @@ process_line <- function(l){
   return(d)
 }
 
-dir_data="/home/evelinamorim/Documentos/UFMG/wordembedding/data/out/"
+dir_data="/home/evelinamorim/Documentos/UFMG/wordembedding/data/out_tmp/"
 
 
 files <- list.files(path=dir_data, pattern="*.txt", full.names=T, recursive=FALSE)
 
 # reading features files
 ptm <- proc.time()
+k <- 10
+doc_size <- seq(k)
 doc_list = lapply(files, function(x) {
    dat = readLines(x)
    l = lapply(dat, process_line)
    nrowl = length(l)
    l <- matrix(unlist(l), nrow = nrowl)
+   fit <- principal.curve(l)
+
+   return(fit)
 })
+ 
+#build X and Y for multilinear regression
+X <- c()
+Y <- c()
+for(d in doc_list){
+  x <- d$s[,c(1)]
+  X <- c(X,x[doc_size])
+  
+  y <- d$s[,c(2)]
+  Y <- c(Y,y[doc_size])
+}
+
+X <- matrix(X, nrow = length(doc_list), byrow = TRUE)
+Y <- matrix(Y, nrow = length(doc_list), byrow = TRUE)
 
 # taking the id from files in order to take the answers in answer vector
 t <- sapply(strsplit(files, split = "docs"), "[",2)
 t <- sapply(strsplit(t,split = ".txt"),"[",1)
 docid_list <- strtoi(t)
 
-#reading ans 
-ans <- read.csv("/home/evelinamorim/Documentos/UFMG/wordembedding/sa/analise/ans.txt")
-
+#reading the answer file (Y variable)
+ans <- read.csv("/home/evelinamorim/Documentos/UFMG/wordembedding/sa/analise/ans.csv", header = FALSE)
+# ans[docid_list]
 proc.time() - ptm
-# length(doc_list)
 
+#satisfaction only from the documents from docid_list
+sat_y <- unlist(ans[1])
+sat_y <- sat_y[docid_list]
+
+# multilinear regression: buidilng model
+reg <- lm( sat_y ~ X + Y)
+
+### TODO ###
+# read the test data
